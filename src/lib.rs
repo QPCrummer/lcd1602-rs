711f9fa36d4f43cf207b6bd59111035f52cbe74d
QPCrummer <66036033+QPCrummer@users.noreply.github.com>
2024-12-07 01:02:38 -0500

    make public

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@
 //! `lcd1602_rs` is a simple embedded-hal driver for a 1602 LCD screen
 #![no_std]
 mod error;
-mod lcd1602;
+pub mod lcd1602;
 
 pub struct LCD1602<EN, RS, D4, D5, D6, D7, Timer> {
     en: EN,
